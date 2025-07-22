046c9ae17012adc75293321f68d1c0f18e203455
Brandon <brancarp93@gmail.com>
2025-07-22 08:31:02 -0400

    update

--- app/components/ItemBox.tsx
+++ app/components/ItemBox.tsx
@@ -3,14 +3,20 @@ import Image from "next/image";
 
 export default function ItemBox(){
     return (
-        <div className="">
+        <div className="flex flex-col justify-center items-center border-1 ">
             <Image
                         src="/prodImages/2_NYLON_SS_ROLLER.jpg"
                         alt="Part"
-                        width={200}
+                        width={150}
           height={150}
         
                       />
+                      {/* Description */}
+                    <div className="flex flex-col items-center border-1 px-2 w-[170px]">
+                        <h1>SN-4-2</h1>
+                        <h1>2" Short Stem Nylon, 4"</h1>
+                        
+                        </div>  
         </div>
     )
 }
